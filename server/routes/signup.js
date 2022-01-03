65465bf265084bb4dea828cf3ea041dcd7274291
Amy <amyliangny@gmail.com>
2022-01-03 14:39:15 -0500

    added query to search by state or park name

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const controller = require('../controllers/controller.js')
 
-router.get('/myparks', async (req, res) => {
-    res.status(200).json({park: res.locals.park})
+router.post('/', async (req, res) => {
+    res.status(200).json({user: res.locals.signup})
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,32 +2,58 @@ require('dotenv/config'); // require the dotenv/config at beginning of file
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+const db = require('./models/db.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Passport config
+require('../config/passport.js')(passport)
+
 // require routers
-const parksRouter = require('./routes/parks');
-const loginRouter = require('./routes/login');
+// const parksRouter = require('./routes/parks');
+// const loginRouter = require('./routes/login');
 const signupRouter = require('./routes/signup');
-const myParksRouter = require('./routes/myparks');
-const profileRouter = require('./routes/profile');
-const parkInfoRouter = require('./routes/parkinfo');
+// const myParksRouter = require('./routes/myparks');
+// const profileRouter = require('./routes/profile');
+// const parkInfoRouter = require('./routes/parkinfo');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', parksRouter);
-app.use('/login', loginRouter);
-app.use('/signup', signupRouter);
-app.use('/myparks', myParksRouter);
-app.use('/profile', profileRouter);
-app.use('/parks/:id', parkInfoRouter);
+// app.use('/', parksRouter);
+// app.use('/login', loginRouter);
+app.use('/api/users/new', signupRouter);
+// app.use('/myparks', myParksRouter);
+// app.use('/profile', profileRouter);
+// app.use('/parks/:id', parkInfoRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'))
   // res.render('home')
 });
+
+//Express-session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, //dont save session if nothing is modified
+  saveUninitialized: true //dont create session until something is stored
+}))
+
+app.get('/google', passport.authenticate('google', {scope: ['profile']}));
+
+app.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), (req, res) => {
+  res.redirect('/dashboard')
+});
+
+
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // what is this one for? 
 // app.get('/park', (req, res) => {
